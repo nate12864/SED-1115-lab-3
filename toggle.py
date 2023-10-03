24279e9e27f3c274700377d14ef1582aa91e34a4
nate12864 <145292726+nate12864@users.noreply.github.com>
2023-10-02 21:15:42 -0400

    Create toggle.py
    Couldn't finish it because of brain fog :(

---
+++ toggle.py
@@ -0,0 +1,12 @@
+from machine import pin
+import time
+
+sw5 = Pin(22, Pin.IN, Pin.PULL_DOWN)
+led1 = Pin(18, Pin.OUT)
+buttonpushed = False
+laststate = False
+
+buttonstate = sw5.value()
+if not buttonstate:
+    time.sleep(0.02)
+    
